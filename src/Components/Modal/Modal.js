8125b390c275e073b888acff39451e4a27098d88
TahsinMostafiz <mostafizur.webdesigner131@gmail.com>
2022-09-24 21:44:58 +0600

    Fixed some error

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 const Modal = (props) => {
-    const {modalData, setModalData} = props
-    const {action, bullet, capacity, category, img, name, price} = modalData
+    
+    const {action, bullet, capacity, category, img, name, price} =props.modalData
     return (
         <div>
             
             <input type="checkbox" id="my-modal-3" className="modal-toggle" />
             <div className="modal">
                 <div className="modal-box relative">
-                    <label onClick={()=> setModalData(null)} htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                    <label onClick={()=> props.setModalData(null)} htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h3 className="text-lg font-bold">{name}</h3>
                     <p className="py-4">{action}, {bullet}, {capacity}, {category}</p>
                     <h4 className='text-1xl font-bold'>Price : {price}</h4>
--- src/Components/Navbar/AllGun/AllGun.js
+++ src/Components/Navbar/AllGun/AllGun.js
@@ -20,7 +20,7 @@ const AllGun = ({increaseCount}) => {
            </div>
            <div className='px-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
                 {
-                    guns.map(gun => <SingleGun gun={gun} increaseCount={increaseCount}></SingleGun>)
+                    guns.map(gun => <SingleGun gun={gun} increaseCount={increaseCount} key={gun.id}></SingleGun>)
                 }
             </div>
         </div>
